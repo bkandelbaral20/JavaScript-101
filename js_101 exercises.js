8c2522ef354e83b5b5f57801be67d8559c05102d
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-09-13 17:26:41 -0500

    101 js

---
+++ js_101 exercises.js
@@ -0,0 +1,19 @@
+//  Exercise 1
+// On the line below, create a variable named onMarsRightNow and assign it the boolean value of false
+
+var onMarsRightNow = false;
+
+//assert(onMarsRightNow, false, "Exercise 1");
+
+// Exercise 2
+// Create a variable named fruits and assign it an array of strings containing the following fruits.
+// mango, banana, guava, kiwi, and strawberry.
+
+var fruits =["mango", "banana", "guava", "kiwi", "strawberry"]
+
+//assert(
+//     fruits,
+//     ["mango", "banana", "guava", "kiwi", "strawberry"],
+//     "Exercise 2"
+// );
+
